503a762fc643a1bbe0133ded8b3e884ff3d33bb4
Henry Catalini Smith <henry@catalinismith.com>
2025-01-06 08:22:05 +0100

    Update names in shell script

--- bump_autoindex.sh
+++ bump_autoindex.sh
@@ -8,7 +8,7 @@ echo "Highest tag: ${highest_tag}"
 cd ..
 
 
-for dir in aml law mbl; do
+for dir in arbetsmiljolagen law medbestammandelagen; do
   if [ -d "$dir" ]; then
     find "$dir" -type f -exec sh -c 'LC_CTYPE=C sed -i "" -E "s/lagstiftning\/autoindex@v[0-9]+/lagstiftning\/autoindex@${1}/g" "$2"' _ "$highest_tag" {} \;
 
